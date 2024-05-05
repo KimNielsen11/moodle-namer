97cc29a3c5550d0bec62970d501b1b0a69f4fdf0
Kim Nielsen <167249686+KimNielsen11@users.noreply.github.com>
2024-05-05 14:46:50 +0200

    Update tutname.py
    Added a clause to delete all onlinetext.html folders

--- tutname.py
+++ tutname.py
@@ -10,13 +10,17 @@ for subdir in os.listdir(dir_path):
     
     # Check if it's a directory
     if os.path.isdir(subdir_path):
-        # Inside each subdirectory, get a list of all files
-        for filename in os.listdir(subdir_path):
-            old_file_path = os.path.join(subdir_path, filename)
-            
-            # Construct the new file name using the subdirectory name and the file extension from the original file name
-            new_file_name = f"{subdir}{os.path.splitext(filename)[1]}"
-            new_file_path = os.path.join(subdir_path, new_file_name)
-            
-            # Rename (move) the file to its new name
-            shutil.move(old_file_path, new_file_path)
+        # If the directory name contains "onlinetext", delete it
+        if "onlinetext" in subdir:
+            shutil.rmtree(subdir_path)
+        else:
+            # Inside each subdirectory, get a list of all files
+            for filename in os.listdir(subdir_path):
+                old_file_path = os.path.join(subdir_path, filename)
+                
+                # Construct the new file name using the subdirectory name and the file extension from the original file name
+                new_file_name = f"{subdir}{os.path.splitext(filename)[1]}"
+                new_file_path = os.path.join(subdir_path, new_file_name)
+                
+                # Rename (move) the file to its new name
+                shutil.move(old_file_path, new_file_path)
